8d75aba8feccf9453972bc15d91d08e6588657be
rejincusi <rejincusi@gmail.com>
2019-09-20 23:40:02 +0200

    Add redux

---
+++ actions/users/sign-up.js
@@ -0,0 +1,31 @@
+import API from '../../lib/api';
+
+import signIn from './sign-in';
+import {
+  API_LOADING,
+  API_READY,
+  API_ERROR,
+} from '../../middleware/api'
+
+const api = new API();
+const users = api.service('users');
+
+export const USER_SIGNUP_ERROR = 'USER_SIGNUP_ERROR';
+
+export default (user) => {
+  return (dispatch) => {
+    dispatch({ type: API_LOADING });
+    users.create(user)
+      .then((result) => {
+        dispatch(signIn(user));
+      })
+      .catch((error) => {
+        dispatch({ type: API_ERROR, payload: error });
+        dispatch({
+          type: USER_SIGNUP_ERROR,
+          payload: error
+        })
+      });
+  }
+
+}
--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,23 +1,71 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { View, KeyboardAvoidingView, Text, TouchableHighlight } from 'react-native';
 import styles from './SignUp.styles';
 import t from 'tcomb-form-native';
 import Person, { formOptions } from '../models/Person';
+import signUp from '../actions/users/sign-up';
 
-export default class SignUp extends Component {
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.onChange = this.onChange.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+
+    this.state = { newUser: null }
+  }
+
+  componentDidMount() {
+    this.refs.form.getComponent('name').refs.input.focus()
+  }
+
+  clearForm() {
+    this.setState({ newUser: null })
+  }
+
+  onChange(newUser) {
+    this.setState({ newUser })
+  }
+
+  onSubmit() {
+    const { form } = this.refs;
+    const newUser = form.getValue();
+    if (!newUser) return;
+    this.props.signUp(newUser);
+    this.clearForm()
+  }
+
   render() {
     const Form = t.form.Form
 
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>Sign up for ShatApp</Text>
+        <KeyboardAvoidingView
+          behavior="padding"
+          style={styles.container}>
+          <Text style={styles.title}>Sign up for ShatApp</Text>
 
-        <Form
-          ref="form"
-          type={Person}
-          options={formOptions} />
+          <Form
+            ref="form"
+            type={Person}
+            options={formOptions}
+            value={this.state.newUser}
+            onChange={this.onChange} />
 
+          <TouchableHighlight
+            style={styles.button}
+            onPress={this.onSubmit}
+            underlayColor='#99d9f4'
+          >
+            <Text style={styles.buttonText}>Sign up</Text>
+          </TouchableHighlight>
+        </KeyboardAvoidingView>
       </View>
     );
   }
 }
+
+const mapStateToProps = ({ loading }) => ({ loading });
+
+export default connect(mapStateToProps, { signUp })(SignUp);
